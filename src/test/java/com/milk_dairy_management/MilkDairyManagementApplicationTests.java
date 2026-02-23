373fe862354a844ba2b834257b8e4b32cb2401b9
Sameer <sameer.sameer185@gmail.com>
2026-02-23 17:58:28 +0530

    Adding Spring Boot Files

---
+++ src/test/java/com/milk_dairy_management/MilkDairyManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.milk_dairy_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MilkDairyManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
